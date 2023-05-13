7278d5e58fd886df001e2cd0e9c948eb5a88fe90
Antti Vesanto <johhni.games@gmail.com>
2023-05-13 22:16:39 +0300

    prettier script file

--- js/script.js
+++ js/script.js
@@ -6,14 +6,15 @@ var collapse_elem1 = document.getElementById("Starter_Guide");
 
 // Check if element is in the viewport
 function isInViewport(elem) {
-  var  rect = elem.getBoundingClientRect();
+  var rect = elem.getBoundingClientRect();
   return (
-      rect.top >= 0 &&
-      rect.bottom >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    rect.top >= 0 &&
+    rect.bottom >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
-};
+}
 
 // Execute a JavaScript when window is being scrolled
 window.onscroll = function () {
@@ -23,36 +24,32 @@ window.onscroll = function () {
 
 // Show button after scrolling 20px down otherwise hide
 function top_buttonFunc() {
-  if (
-    document.documentElement.scrollTop > 20
-  ) {
+  if (document.documentElement.scrollTop > 20) {
     top_button.style.display = "block";
   } else {
     top_button.style.display = "none";
   }
-};
+}
 
- // Define location of the top_button
- // If body_bottom is visible in the viewport button is placed 10px on top of the bottom bar
+// Define location of the top_button
+// If body_bottom is visible in the viewport button is placed 10px on top of the bottom bar
 function body_bottomFunc() {
-  if (
-      isInViewport(body_bottom)
-  ) {
-      top_button.style.position = "absolute";
-      top_button.style.bottom = "auto";
-      top_button.style.top = body_bottom.offsetTop - 35 +"px";
+  if (isInViewport(body_bottom)) {
+    top_button.style.position = "absolute";
+    top_button.style.bottom = "auto";
+    top_button.style.top = body_bottom.offsetTop - 35 + "px";
   } else {
-      top_button.style.position = "fixed";
-      top_button.style.bottom = "10px";
-      top_button.style.top = "auto";
+    top_button.style.position = "fixed";
+    top_button.style.bottom = "10px";
+    top_button.style.top = "auto";
   }
-};
+}
 
 // At the firs time loading page. Check if content is defined
 // If content isn't defined. Redirect to home page
-if(window.location.href.split("?")[1] == undefined) {
+if (window.location.href.split("?")[1] == undefined) {
   window.location.assign("?home#");
-};
+}
 
 // Load and place page content to the placeholders
 // Navigation bar
@@ -60,17 +57,22 @@ $("#navbar_placeholder").load("assets/navbar.html");
 
 // Content
 // Define site content usign domain's query string
-$("#content_placeholder").load("pages/"+location.href.split("?")[1].split("#")[0]+".html", function() {
-  $("#Starter_Guide, #Five_Tips").on("show.bs.collapse", function() {
-    setTimeout(() => {$('html, body').scrollTop($(this).offset().top);}, 500);
-  });
-});
+$("#content_placeholder").load(
+  "pages/" + location.href.split("?")[1].split("#")[0] + ".html",
+  function () {
+    $("#Starter_Guide, #Five_Tips").on("show.bs.collapse", function () {
+      setTimeout(() => {
+        $("html, body").scrollTop($(this).offset().top);
+      }, 500);
+    });
+  }
+);
 
 // Bottom bar
 $("#bottom_bar_placeholder").load("assets/bottom_bar.html");
 
 // Footer
-$("#footer_placeholder").load("assets/footer.html", function() {
+$("#footer_placeholder").load("assets/footer.html", function () {
   // Manage the navigation bar and footer bar
   // Active page indicator
   // Remove "active"-class from all links
@@ -78,20 +80,17 @@ $("#footer_placeholder").load("assets/footer.html", function() {
 
   // Define current page using domain's queary string
   // Add "active"-class to its link
-  $("#"+location.href.split("?")[1].slice(0,-1)).addClass("active");
+  $("#" + location.href.split("?")[1].slice(0, -1)).addClass("active");
 
   // Define current page and set redirect pages for forms
   for (let i = 0; i < rUrls.length; i++) {
     rUrls[i].setAttribute("value", window.location.href);
-  };
+  }
 
   // Add js files to the page
   function JS(jsfile) {
     var elem = document.createElement("script");
     elem.setAttribute("src", jsfile);
     document.getElementsByTagName("body")[0].appendChild(elem);
-  };
-
-  // FSLightBox to the page
-  JS("/js/fs/fslightbox.js");
+  }
 });
